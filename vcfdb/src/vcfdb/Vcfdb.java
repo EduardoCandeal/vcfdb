a13ad04f9804a4cc05f02f5a4c15946df8e2050d
ecandeal <ecandeal@maiz>
2016-04-05 13:11:40 +0200

    cambios

--- vcfdb/src/vcfdb/Vcfdb.java
+++ vcfdb/src/vcfdb/Vcfdb.java
@@ -6,7 +6,10 @@
 
 package vcfdb;
 import java.util.Scanner;
-
+import java.io.*;
+import picard.PicardException;
+import picard.vcf.*;
+import picard.*;
 /*
  *
  * @author ecandeal
@@ -16,8 +19,29 @@ public class Vcfdb {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws FileNotFoundException, IOException {
         System.out.println("prueba2");
-    }
+        File file = new File("/home/ecandeal/NetBeansProjects/vcfdb/test/test1.vcf");
+        File file2 = new File("/home/ecandeal/NetBeansProjects/vcfdb/test/test2.vcf");
+        FileReader fr2= new FileReader(file);
+       FileInputStream fis = new FileInputStream(file);
+       SortVcf are= new SortVcf();
+       are.INPUT.add(file);
+       are.INPUT.add(file2);
+       are.getCommandLine();
+       GenotypeConcordance asd= new GenotypeConcordance();
+      
+       
+        System.out.println(are.getCommandLine());
+     
+        //System.out.println(fis.available());
+        //System.out.println(fr2.read());
+     
+        
+ 
+        
+        //System.out.println(as.INPUT.length());
+        
+    } 
     
 }
